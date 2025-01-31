5bf66ddb61c8a8c835c7914c0e92653fde34bd1d
Poway <n.smoliar2013@gmail.com>
2025-01-31 17:04:53 +0200

    Initial Commit

---
+++ MVVM+C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM+C
+//
+//  Created by Никита on 31.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
