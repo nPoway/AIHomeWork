47776829a7bea95f6d04eac6a7a4385fc1e09377
Poway <n.smoliar2013@gmail.com>
2025-05-09 13:42:42 +0300

    init commit

--- AIHomeWork/Utilities/GlobalConstants.swift
+++ AIHomeWork/Utilities/GlobalConstants.swift
@@ -4,9 +4,7 @@ import SwiftUI
 let screenSize = UIScreen.main.bounds
 let iphoneWithButton = screenSize.height <= 736
 
-//let gptAPIKey = "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"
-
-let gptAPIKey = "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"
+let gptAPIKey = "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"
 
 
 
